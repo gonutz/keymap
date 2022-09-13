e76c7cd9cfeb6db9260a215408ea6f38431ef41d
lars <gonutz@fake.mail>
2022-09-13 10:57:26 +0200

    Switch layers with tab or hold

--- keymap.c
+++ keymap.c
@@ -127,19 +127,16 @@
 #define BkTik DE_GRV
 #define Prvat RCS(DE_N)
 #define ToBas TO(_BASE)
-#define ToNav TT(_NAV)
-#define ToNum TT(_NUM)
-// TODO Bring back umlauts.
-#define ToUml KC_NO // TO(_UMLAUTS)
+#define ToUml TO(_UMLAUTS)
 #define ToQrz TO(_QWERTZ)
 
-enum layers { _BASE, _SYMBOLS, _NUM, _NAV, _UMLAUTS, _QWERTZ };
+enum layers { _BASE, _SYMBOLS, _NAV, _NUM, _UMLAUTS, _QWERTZ };
 
 enum custom_keycodes {
 	SayGG = SAFE_RANGE,
 	ToSym,
-	//ToNav,
-	//ToNum,
+	ToNav,
+	ToNum,
 };
 
 int current_layer(void) {
@@ -149,32 +146,44 @@ int current_layer(void) {
 	return _BASE;
 }
 
+bool is_layer_switch_key(uint16_t keycode) {
+	return keycode == ToSym || keycode == ToNav || keycode == ToNum;
+}
+
+int layer_for_key(uint16_t keycode) {
+	if(keycode == ToNav)
+		return _NAV;
+	if(keycode == ToNum)
+		return _NUM;
+	return _SYMBOLS;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	static int last_layer = 0;
-	static bool stay_in_symbols = false;
-	static bool non_symbol_key_pressed = false;
+	static bool stay_in_layer = false;
+	static bool non_layer_switch_key_pressed = false;
 
 	int layer = current_layer();
 
-	if(keycode == ToSym) {
+	if(is_layer_switch_key(keycode)) {
 		if(record->event.pressed) {
 			// Symbol key down.
-			if(layer != _SYMBOLS) {
+			if(layer != layer_for_key(keycode)) {
 				last_layer = layer;
-				layer_move(_SYMBOLS);
-				stay_in_symbols = false;
-				non_symbol_key_pressed = false;
+				layer_move(layer_for_key(keycode));
+				stay_in_layer = false;
+				non_layer_switch_key_pressed = false;
 			}
 		} else {
 			// Symbol key up.
-			if(non_symbol_key_pressed && !stay_in_symbols)
+			if(non_layer_switch_key_pressed && !stay_in_layer)
 				layer_move(last_layer);
 			else
-				stay_in_symbols = true;
+				stay_in_layer = true;
 		}
 		return false;
-	} else
-		non_symbol_key_pressed = true;
+	} else if(keycode != ToUml)
+		non_layer_switch_key_pressed = true;
 
 	if(keycode == SayGG && record->event.pressed) {
 		SEND_STRING("\ngg\n");
@@ -186,40 +195,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_planck_mit(
-    Boot_ , __Q__ , __W__ , __F__ , __P__ , _____ , _____ , __L__ , __U__ , __Y__ , __V__ , _____ ,
+    Boot_ , __Q__ , __W__ , __F__ , __P__ , _____ , _____ , __L__ , __U__ , __Y__ , __V__ , ToBas ,
     ToSym , __A__ , __R__ , __S__ , __T__ , __G__ , __M__ , __N__ , __E__ , __I__ , __O__ , Gui_E ,
     _____ , __Z__ , __X__ , __C__ , __D__ , _____ , _____ , __H__ , __K__ , __B__ , __J__ , _____ ,
     _____ , _____ , _____ , ToNav , C_Bck ,     Shift     , C_Spc , ToNum , _____ , _____ , ToQrz
 ),
 [_SYMBOLS] = LAYOUT_planck_mit(
-    Boot_ , Tilde , _At__ , Hash_ , Equal , _____ , _____ , Quote , DblQu , Qstin , Minus , _____ ,
+    Boot_ , Tilde , _At__ , Hash_ , Equal , _____ , _____ , Quote , DblQu , Qstin , Minus , ToBas ,
     ToSym , Dolar , Perct , Ampsn , ToUml , Bkslh , Bang_ , LParn , RParn , Comma , _Dot_ , Enter ,
     _____ , _Bar_ , Less_ , More_ , Under , _____ , _____ , LBrck , RBrck , LBrce , RBrce , _____ ,
     _____ , _____ , _____ , ToNav , C_Bck ,     ToBas     , C_Spc , ToNum , _____ , _____ , ToQrz
 ),
-[_NUM] = LAYOUT_planck_mit(
-    Boot_ , _F12_ , _F_7_ , _F_8_ , _F_9_ , _____ , _____ , __7__ , __8__ , __9__ , Ctl_0 , _____ ,
-    ToSym , _F11_ , _F_4_ , _F_5_ , GuiF6 , Mult_ , Slash , __4__ , __5__ , __6__ , Plus_ , Enter ,
-    _____ , _F10_ , _F_1_ , _F_2_ , _F_3_ , _____ , _____ , __1__ , __2__ , __3__ , _Dot_ , _____ ,
-    _____ , _____ , _____ , ToNav , C_Bck ,     ToBas     , C_Spc , ToNum , _____ , _____ , ToQrz
-),
 [_NAV] = LAYOUT_planck_mit(
-    Boot_ , ReOpn , Wndws , _Esc_ , Prvat , _____ , _____ , PagUp , _Up__ , PgDwn , Quit_ , _____ ,
+    Boot_ , ReOpn , Wndws , _Esc_ , Prvat , _____ , _____ , PagUp , _Up__ , PgDwn , Quit_ , ToBas ,
     ToSym , Ctrl_ , Shift , _Alt_ , _Tab_ , _Gui_ , Home_ , Left_ , Down_ , Right , _End_ , Enter ,
     _____ , _Del_ , _Cut_ , Copy_ , Paste , _____ , _____ , L_Tab , R_Tab , NwTab , ClTab , _____ ,
     _____ , _____ , _____ , ToNav , C_Bck ,     ToBas     , C_Spc , ToNum , _____ , _____ , ToQrz
 ),
+[_NUM] = LAYOUT_planck_mit(
+    Boot_ , _F12_ , _F_7_ , _F_8_ , _F_9_ , _____ , _____ , __7__ , __8__ , __9__ , Ctl_0 , ToBas ,
+    ToSym , _F11_ , _F_4_ , _F_5_ , GuiF6 , Mult_ , Slash , __4__ , __5__ , __6__ , Plus_ , Enter ,
+    _____ , _F10_ , _F_1_ , _F_2_ , _F_3_ , _____ , _____ , __1__ , __2__ , __3__ , _Dot_ , _____ ,
+    _____ , _____ , _____ , ToNav , C_Bck ,     ToBas     , C_Spc , ToNum , _____ , _____ , ToQrz
+),
 [_UMLAUTS] = LAYOUT_planck_mit(
-    Boot_ , Cmflx , BkTik , _Alt_ , Print , _____ , _____ , Play_ , _Ue__ , _____ , _____ , _____ ,
+    Boot_ , Cmflx , BkTik , _Alt_ , Print , _____ , _____ , Play_ , _Ue__ , _____ , _____ , ToBas ,
     ToSym , _Ae__ , _____ , _Ss__ , ToUml , _____ , _____ , VolDn , VolUp , Mute_ , _Oe__ , Enter ,
     _____ , _____ , _____ , _____ , _____ , _____ , _____ , Darkr , Brght , _____ , _____ , _____ ,
     _____ , _____ , _____ , ToNav , C_Bck ,     ToBas     , C_Spc , ToNum , _____ , _____ , ToQrz
 ),
 [_QWERTZ] = LAYOUT_planck_mit(
-    Boot_ , SayGG , __Q__ , __W__ , __E__ , _____ , _____ , _____ , _____ , _____ , _____ , _____ ,
+    Boot_ , SayGG , __Q__ , __W__ , __E__ , _____ , _____ , _____ , _____ , _____ , _____ , ToBas ,
     _Esc_ , Shift , __A__ , __S__ , __D__ , Enter , _____ , _____ , _____ , _____ , _____ , _____ ,
     _____ , _Alt_ , __C__ , __C__ , __C__ , _____ , _____ , _____ , _____ , _____ , _____ , _____ ,
-    _____ , _____ , _____ , __Y__ , Space ,     Space     , _____ , _____ , _____ , _____ , ToBas
+    _____ , _____ , _____ , __Y__ , Space ,     Space     , _____ , _____ , _____ , _____ , ToQrz
 ),
 
 };
